685c6e805ea1d3983847908d0367e7c1f7f725fc
samyss-dev <127264748+samyss-dev@users.noreply.github.com>
2023-11-12 00:51:24 -0300

    Update IsPublic.ts

--- src/shared/decorators/IsPublic.ts
+++ src/shared/decorators/IsPublic.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 
-export const IsPublic = () => SetMetadata('isPublic', true);
+export const IsPublic = () => SetMetadata(IS_PUBLIC_KEY, true);
